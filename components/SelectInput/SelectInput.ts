8912ed1689ba0b93be30d38cd97d4e339c5d28ad
NetFeez <netfeez.dev@gmail.com>
2026-05-07 03:30:29 +0000

    refactor: update SelectInput constructor to use options object and improve placeholder handling

--- components/SelectInput/SelectInput.ts
+++ components/SelectInput/SelectInput.ts
@@ -1,25 +1,28 @@
 import { Element, Component } from 'vizui';
+import Utilities from '../Utilities';
 
 export class SelectInput<options extends string[] = string[]> extends Component<'select', SelectInput.EventMap<options>> {
     static { this.css.load('${basicComponents}/SelectInput/SelectInput.css'); }
 
     protected root: Element<"select">;
-    protected placeholder: string;
-    public constructor(options: options, placeholder: string = 'select') { super();
-        this.placeholder = placeholder;
+    protected vPlaceholder: string;
 
-        this.root = Element.new('select').setAttributes({
-            name: 'select',
-            class: 'selectInput'
-        }).append(
-            Element.new('option', this.placeholder),
-            ...options.map(option => Element.new('option', option))
-        );
+    public constructor(optionList: options, options: SelectInput.Options = {}) { super();
+        const { placeholder = 'select', ...identity } = options;
+
+        this.vPlaceholder = placeholder;
+
+        const optionElements = [];
+        optionElements.push(Element.new('option', placeholder).setAttribute('disabled', 'true'));
+        optionElements.push(...optionList.map(option => Element.new('option', option)));
+
+        this.root = Element.new('select', null, { name: 'select', class: 'selectInput' });
+        this.root.append(...optionElements);
 
         this.root.on('change', () => this.emit('submit', this.getSelected()));
     }
     public getSelected(): options[number] {
-        if (this.root.root.value == this.placeholder) return '';
+        if (this.root.root.value == this.vPlaceholder) return '';
         return this.root.root.value;
     }
     public setSelected(option: options[number]) {
@@ -30,6 +33,9 @@ export class SelectInput<options extends string[] = string[]> extends Component<
 export namespace SelectInput {
     export type EventMap<options extends string[] = string[]> = {
         submit: [selected: options[number]];
+    }
+    export interface Options extends Omit<Utilities.Identity, 'for'> {
+        placeholder?: string;
     }
 }
 
