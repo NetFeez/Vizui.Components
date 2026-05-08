ed21b448bacefad38976774ca6f2a07d980120c4
NetFeez <netfeez.dev@gmail.com>
2026-05-08 05:29:22 +0000

    fix: update build script to use consistent output file and correct npm commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if npx dep install > .compile.log 2>&1; then
+set -eo pipefail
+outputFile="output.log"
+
+if npx dep install > $outputFile 2>&1; then
     echo -e "\x1B[32mDependencies installed successfully.\x1B[0m"
 else
     echo -e "\x1B[31mFailed to install \x1B[36]'depflow'\x1B[31m dependencies.\x1B[0m"
-    tail .compile.log
+    tail $outputFile
     exit 1
 fi
 
-if npx tsc --pretty > .compile.log 2>&1; then
+if npx tsc > $outputFile 2>&1; then
     echo -e "\x1B[32mTypeScript compiled successfully.\x1B[0m"
 else
     echo -e "\x1B[31mFailed to compile TypeScript.\x1B[0m"
-    tail .compile.log
-  exit 1
+    tail $outputFile
+    exit 1
 fi
+
+rm -f $outputFile
