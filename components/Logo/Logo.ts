21b4adf1d903bf071d006990d679bf4480b1e24c
NetFeez <netfeez.dev@gmail.com>
2026-08-23 00:19:08 +0000

    refactor(logo): enforce coding conventions
    - Add file header, readonly UI members and normalized quotes.
    - Rename eventMap namespace type to EventMap with proper semicolon.

--- components/Logo/Logo.ts
+++ components/Logo/Logo.ts
@@ -1,12 +1,18 @@
+/**
+ * @author NetFeez <netfeez.dev@gmail.com>
+ * @description Clickable logo component with text, image and an optional URL.
+ * @license Apache-2.0
+ */
+
 import { Component, Element } from 'vizui';
 
-export class Logo extends Component<'button', Logo.eventMap> {
-    static { this.css.load('${basicComponents}/Logo/Logo.css'); }
+export class Logo extends Component<'button', Logo.EventMap> {
+    static { this.css.load('{{base}}/Logo/Logo.css'); }
 
-    protected root: Element<"button">;
+    protected root: Element<'button'>;
 
-    protected eText: Element<'p'>;
-    protected eImage: Element<'img'>;
+    protected readonly eText: Element<'p'>;
+    protected readonly eImage: Element<'img'>;
 
     protected vUrl: string | null;
 
@@ -16,15 +22,16 @@ export class Logo extends Component<'button', Logo.eventMap> {
      * @param image - The source URL of the logo image.
      * @param url - An optional URL to navigate to when the logo is clicked.
      */
-    public constructor(text: string, image: string, url: string | null = null) { super();
+    public constructor(text: string, image: string, url: string | null = null) {
+        super();
         this.vUrl = url;
         this.root = Element.new('button', null, { class: 'Logo' });
         this.eImage = Element.new('img', null, { class: 'image', src: image || '' });
         this.eText = Element.new('p', text, { class: 'text' });
         this.root.append(this.eImage, this.eText);
 
-        this.root.on('click', () => this.emit('click', this.vUrl));
-        this.root.on('mouseover', () => this.emit('hover', this.vUrl));
+        this.root.on('click', (e) => this.emit('click', this.vUrl, e));
+        this.root.on('mouseover', (e) => this.emit('hover', this.vUrl, e));
     }
     public get text(): string { return this.eText.text; }
     public set text(text: string) { this.eText.text = text; }
@@ -34,9 +41,9 @@ export class Logo extends Component<'button', Logo.eventMap> {
     public set image(image: string) { this.eImage.setAttribute('src', image); }
 }
 export namespace Logo {
-    export type eventMap = {
-        click: [url: string | null];
-        hover: [url: string | null];
-    }
+    export type EventMap = {
+        click: [url: string | null, event: Event];
+        hover: [url: string | null, event: Event];
+    };
 }
 export default Logo;
