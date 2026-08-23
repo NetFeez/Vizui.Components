c6381660f4d5ea89863aa0222c42e8c13e9fbdbf
NetFeez <netfeez.dev@gmail.com>
2026-08-23 00:15:39 +0000

    refactor(button): enforce coding conventions
    - Add file header and normalize generic element quotes.
    - Rename public namespace types eventMap/options to EventMap/Options.
    - Mark eText/eImage as readonly and expand constructor to multiline form.

--- components/Button/Button.ts
+++ components/Button/Button.ts
@@ -1,21 +1,27 @@
+/**
+ * @author NetFeez <netfeez.dev@gmail.com>
+ * @description Button component with label text and an optional leading image.
+ * @license Apache-2.0
+ */
+
 import { Component, Element } from 'vizui';
 
-import Utilities from '..//Utilities.js';
+import Utilities from '../Utilities.js';
 
-export class Button extends Component<'button', Button.eventMap> {
-    static { this.css.load('${basicComponents}/Button/Button.css'); }
+export class Button extends Component<'button', Button.EventMap> {
+    static { this.css.load('{{base}}/Button/Button.css'); }
 
-    protected root: Element<"button">;
+    protected root: Element<'button'>;
 
-    private eText: Element<'span'>;
-    private eImage: Element<'img'>;
+    private readonly eText: Element<'span'>;
+    private readonly eImage: Element<'img'>;
 
     /**
      * Create a button component with text and an optional image.
      * @param text - The text to display on the button.
      * @param options - Optional configuration for the button, including id, class, and image.
      */
-    public constructor(text: string, options: Button.options = {}) { super();
+    public constructor(text: string, options: Button.Options = {}) { super();
         this.root = Element.new('button', null, { class: 'Button' });
 
         this.eText = Element.new('span', text);
@@ -25,15 +31,15 @@ export class Button extends Component<'button', Button.eventMap> {
 
         this.root.append(this.eImage, eTextContainer);
 
-        this.root.on('click', () => this.emit('click'));
-        this.root.on('mouseover', () => this.emit('hover'));
+        this.root.on('click', (e) => this.emit('click', e));
+        this.root.on('mouseover', (e) => this.emit('hover', e));
 
         Utilities.setIdentity(this, options);
         this.image = options.image || null;
     }
     public get text(): string { return this.eText.text; }
     public set text(text: string) { this.eText.text = text; }
-    public get image(): string { return this.eImage.root.src }
+    public get image(): string { return this.eImage.root.src; }
     public set image(value: string | null) {
         this.eImage.root.src = value || '';
         if (!value) this.eImage.remove();
@@ -41,14 +47,14 @@ export class Button extends Component<'button', Button.eventMap> {
     }
 }
 export namespace Button {
-    export type eventMap = {
-        click: [];
-        hover: [];
-    }
-    export type options = {
+    export type EventMap = {
+        click: [e: Event];
+        hover: [e: Event];
+    };
+    export type Options = {
         id?: string;
         class?: string;
         image?: string;
-    }
+    };
 }
 export default Button;
